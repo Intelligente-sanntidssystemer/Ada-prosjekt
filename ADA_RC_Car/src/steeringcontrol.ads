570eda3b9edc24196f4756189e96b0cb943dfdfe
Andrelet <71266549+Andrelet@users.noreply.github.com>
2020-11-13 15:43:11 +0100

    Update steeringcontrol.ads

--- ADA_RC_Car/src/steeringcontrol.ads
+++ ADA_RC_Car/src/steeringcontrol.ads
@@ -1,15 +1,15 @@
 with NRF52_DK.IOs;
 
-package SteeringControl is
+package SteeringControl is --Servo and Motor controls --
    
-   procedure Servocontrol(ServoPin : NRF52_DK.IOs.Pin_Id; Value : NRF52_DK.IOs.Analog_Value);
+   procedure Servocontrol(ServoPin : NRF52_DK.IOs.Pin_Id; Value : NRF52_DK.IOs.Analog_Value); --- Servo--
    
-   procedure Direction_Controller;
+   procedure Direction_Controller; --Left or right write to servo to turn car --
    
-   procedure Motor_Controller;
+   procedure Motor_Controller; --Motor forward/backward --
    
-   procedure Crash_Stop_Forward;
+   procedure Crash_Stop_Forward; --When triggering emergency stop --
    
-   procedure Crash_Stop_Backward;
+   procedure Crash_Stop_Backward; --When triggering emergency stop --
    
 end SteeringControl;
