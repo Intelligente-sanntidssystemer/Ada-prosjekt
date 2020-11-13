a31fbb6579813341cb7665c8274759bb440c4f77
Andrelet <71266549+Andrelet@users.noreply.github.com>
2020-11-13 20:13:09 +0100

    Update car_priorities.adb

--- ADA_RC_Car/src/car_priorities.adb
+++ ADA_RC_Car/src/car_priorities.adb
@@ -12,26 +12,29 @@ package body car_priorities is
    Period_Time : NRF52_DK.Time.Time_Ms := NRF52_DK.Time.Clock;
      
    task body Emergency_Stop is
-      Max_Distance : Float := 100.0; --Max distance which is 100cm.
+   Deadline_Distance : Float := 100.0; --Max distance which is 100cm 
+   
    begin
       loop
          --This is the highest priority task
          --The idea here is to put a delay until Next_Deadline which is the Clock function
          --In NRF52_DK.Time package and at the end of the function we update the Next_Deadline
          --with the time spent to run this loop
-         delay until Next_Deadline;
-         Period_Time := NRF52_DK.Time.Clock;
-         while Sensor.HCSR04_Distance < Max_Distance loop
+      delay until Next_Deadline; -- SENSOR HAS HARD DEADLINE --
+      Period_Time := NRF52_DK.Time.Clock;
+      if Sensor.HCSR04_Distance < Deadline_Distance then -- Emergency stop condition activated --
+         while Sensor.HCSR04_Distance < Deadline_Distance loop -- if distance front is too close, stop running forward
             SteeringControl.Crash_Stop_Forward; 
          end loop;
          
-         while sensor_behind.HCSR04_Behind_Distance < Max_Distance loop
+         while sensor_behind.HCSR04_Behind_Distance < Deadline_Distance loop -- If back distance is too close, stop running backwards
             SteeringControl.Crash_Stop_Backward;
          end loop;
-         Next_Deadline := Next_Deadline + (NRF52_DK.Time.Clock - Period_Time);
-         
+      end if;
+      Next_Deadline := Next_Deadline + (NRF52_DK.Time.Clock - Period_Time);
       end loop;
    end Emergency_Stop;
+   
    
    task body Motor_Steering is
    begin
