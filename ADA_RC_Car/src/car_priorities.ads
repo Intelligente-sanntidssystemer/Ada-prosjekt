97e465c10c59d42a898049e7ef7e65bc4be8eea3
Andrelet <71266549+Andrelet@users.noreply.github.com>
2020-11-13 15:44:17 +0100

    Update car_priorities.ads

--- ADA_RC_Car/src/car_priorities.ads
+++ ADA_RC_Car/src/car_priorities.ads
@@ -1,7 +1,7 @@
 package car_priorities is
-   task type Direction_Steering;
-   task type Motor_Steering;
-   task type Emergency_Stop;
+   task type Direction_Steering; --Turning--
+   task type Motor_Steering; --Forward/Backward --
+   task type Emergency_Stop; --Sensor and distance checking --
    
 
 end car_priorities;
