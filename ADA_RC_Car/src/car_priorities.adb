668cc66b69335ba5a93eddb22cea85f5c290481f
ZhinXV <70554676+ZhinXV@users.noreply.github.com>
2020-11-13 15:20:16 +0100

    Update car_priorities.adb

--- ADA_RC_Car/src/car_priorities.adb
+++ ADA_RC_Car/src/car_priorities.adb
@@ -15,6 +15,10 @@ package body car_priorities is
       Deadline_Distance : Float := 100.0; --Deadline distance which is 100cm.
    begin
       loop
+         --This is the highest priority task
+         --The idea here is to put a delay until Next_Deadline which is the Clock function
+         --In NRF52_DK.Time package and at the end of the function we update the Next_Deadline
+         --with the time spent to run this loop
          delay until Next_Deadline;
          Period_Time := NRF52_DK.Time.Clock;
          while Sensor.HCSR04_Distance < Deadline_Distance loop
@@ -32,6 +36,9 @@ package body car_priorities is
    task body Motor_Steering is
    begin
       loop
+         --This is supposed to be the 2nd highest priority task, ideally this will run
+         --after the Emergency_Stop and update the Next_Deadline at the end of the loop which
+         --carries over to the next task.
          delay until Next_Deadline;
          Period_Time := NRF52_DK.Time.Clock;
          SteeringControl.Motor_Controller;
@@ -43,6 +50,8 @@ package body car_priorities is
    task body Direction_Steering is
    begin
       loop
+         --The lowest priority task we have currently and this will run after the other two
+         --higher priority tasks with an update to Next_Deadline at the end. 
          delay until Next_Deadline;
          Period_Time := NRF52_DK.Time.Clock;
          SteeringControl.Direction_Controller;
