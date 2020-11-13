f2bb6bd8e7829fff1c9b5576567e64ed77fa9369
ZhinXV <70554676+ZhinXV@users.noreply.github.com>
2020-11-13 15:21:25 +0100

    Update main.adb

--- ADA_RC_Car/src/main.adb
+++ ADA_RC_Car/src/main.adb
@@ -2,6 +2,7 @@ with NRF52_DK.IOs; use NRF52_DK.IOs;
 with SteeringControl; use SteeringControl;
 with NRF52_DK.Buttons; use NRF52_DK.Buttons;
 with NRF52_DK.Time;
+with car_priorities;
 
 procedure Main is
 
